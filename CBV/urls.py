5077d4f0cf2de7a7501717cd43909549fcc3d336
uzairdurani <muhammaduzairdurani@gmail.com>
2022-02-09 09:00:21 -0800

    detail views

--- CBV/urls.py
+++ CBV/urls.py
@@ -1,11 +1,13 @@
+from re import template
 from django.views.generic import TemplateView
 from django.urls import path
-from .views import newPage,PostPreLoadView,SinglePostView
+from .views import newPage,PostPreLoadView,SinglePostView,modelDetailView
 app_name = 'cbv'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html',
                                   extra_context={'title': 'This is custom context'}), name='home'),
     path('new',newPage.as_view(),name = 'newPage'),
+    path('<slug:pk>',modelDetailView.as_view(),name = 'detail_views'),
     path('page3/<int:pk>',PostPreLoadView.as_view(),name = 'page3'),
     path('page4/<int:pk>',SinglePostView.as_view(),name = 'page4'),
 ]
--- CBV/views.py
+++ CBV/views.py
@@ -1,9 +1,10 @@
+from pyexpat import model
 from django.shortcuts import render
 from django.views.generic.base import TemplateView,RedirectView
 from .models import student,Post
 from django.shortcuts import get_object_or_404
 from django.db.models import F
-
+from django.views.generic import DetailView
 # Create your views here.
 class newPage(TemplateView):
     template_name = 'newPage.html'
@@ -28,3 +29,9 @@ class SinglePostView(TemplateView):
         context =  super().get_context_data(**kwargs)
         context['posts'] = get_object_or_404(Post, id = self.kwargs.get('pk'))
         return context
+
+
+class modelDetailView(DetailView):
+  model = student
+  template_name = 'student_detail.html'
+  
